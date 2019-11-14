012aa7b80885a04fa9ff3c0c1afb10ea9921ab6a
Anna Epishova <epishova@google.com>
2019-11-14 12:12:47 -0500

    Changed code formatting

--- udfs/migration/redshift/interval_literal_to_seconds.sql
+++ udfs/migration/redshift/interval_literal_to_seconds.sql
@@ -14,28 +14,28 @@
  * limitations under the License.
  */
 
-CREATE OR REPLACE FUNCTION
-  rs.interval_literal_to_seconds(interval_literal STRING) AS ( /* For intervals in Redshift, there are 360 days in a year. */ (
+CREATE OR REPLACE FUNCTION rs.interval_literal_to_seconds(interval_literal STRING) 
+AS ( 
+  /* For intervals in Redshift, there are 360 days in a year. */ 
+  (
     SELECT
-      SUM(CASE
-          WHEN unit IN ('minutes', 'minute', 'm' ) THEN num * 60
-          WHEN unit IN ('hours',
-          'hour',
-          'h' ) THEN num * 60 * 60
-          WHEN unit IN ('days', 'day', 'd' ) THEN num * 60 * 60 * 24
-          WHEN unit IN ('weeks',
-          'week',
-          'w' ) THEN num * 60 * 60 * 24 * 7
-          WHEN unit IN ('months', 'month' ) THEN num * 60 * 60 * 24 * 30
-          WHEN unit IN ('years',
-          'year') THEN num * 60 * 60 * 24 * 360
-        ELSE
-        num
-      END
-        )
-    FROM (
-      SELECT
-        CAST(REGEXP_EXTRACT(value, r'^[0-9]*\.?[0-9]+') AS numeric) num,
-        SUBSTR(value, LENGTH(REGEXP_EXTRACT(value, r'^[0-9]*\.?[0-9]+')) + 1) unit
-      FROM
-        UNNEST(SPLIT(REPLACE(interval_literal, ' ', ''), ',')) value )) );
+      SUM(
+          CASE
+            WHEN unit IN ('minutes', 'minute', 'm') THEN num * 60
+            WHEN unit IN ('hours', 'hour', 'h') THEN num * 60 * 60
+            WHEN unit IN ('days', 'day', 'd') THEN num * 60 * 60 * 24
+            WHEN unit IN ('weeks','week', 'w') THEN num * 60 * 60 * 24 * 7
+            WHEN unit IN ('months', 'month') THEN num * 60 * 60 * 24 * 30
+            WHEN unit IN ('years', 'year') THEN num * 60 * 60 * 24 * 360
+            ELSE num
+            END)
+    FROM 
+      (
+        SELECT
+          CAST(REGEXP_EXTRACT(value, r'^[0-9]*\.?[0-9]+') AS numeric) num,
+          SUBSTR(value, LENGTH(REGEXP_EXTRACT(value, r'^[0-9]*\.?[0-9]+')) + 1) unit
+        FROM
+          UNNEST(SPLIT(REPLACE(interval_literal, ' ', ''), ',')) value 
+      )
+  ) 
+);
