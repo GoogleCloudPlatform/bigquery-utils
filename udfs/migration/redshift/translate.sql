6ff7114beeb33353cf6d98713d576336c2caed55
Anna Epishova <epishova@google.com>
2019-11-14 11:41:36 -0500

    Changed code formatting

--- udfs/migration/redshift/translate.sql
+++ udfs/migration/redshift/translate.sql
@@ -14,34 +14,10 @@
  * limitations under the License.
  */
 
-CREATE OR REPLACE FUNCTION
-  rs.translate(expression STRING,
-    characters_to_replace STRING,
-    characters_to_substitute STRING) AS (
-  IF
-    (LENGTH(characters_to_replace) < LENGTH(characters_to_substitute)
-      OR LENGTH(expression) < LENGTH(characters_to_replace),
-      expression,
-      (
-      SELECT
-        STRING_AGG( IFNULL( (
-            SELECT
-              ARRAY_CONCAT([c], SPLIT(characters_to_substitute, ''))[SAFE_OFFSET((
-                SELECT
-                  IFNULL(MIN(o2) + 1,
-                    0)
-                FROM
-                  UNNEST(SPLIT(characters_to_replace, '')) AS k
-                WITH
-                OFFSET
-                  o2
-                WHERE
-                  k = c))] ),
-            ''), ''
-        ORDER BY
-          o1)
-      FROM
-        UNNEST(SPLIT(expression, '')) AS c
-      WITH
-      OFFSET
-        o1 )) );
+CREATE OR REPLACE FUNCTION rs.translate(
+  expression STRING,
+  characters_to_replace STRING,
+  characters_to_substitute STRING) 
+AS (
+  bqutil.fn.translate(expression, characters_to_replace, characters_to_substitute)
+);
