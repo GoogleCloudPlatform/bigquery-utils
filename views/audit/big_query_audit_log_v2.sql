0bd63778768a71ce0a85885479f21526d7698013
Namrata S <16926363+NamrataShah5@users.noreply.github.com>
2020-03-12 14:20:26 -0500

    Update big_query_audit_log_v2.sql

--- views/audit/big_query_audit_log_v2.sql
+++ views/audit/big_query_audit_log_v2.sql
@@ -87,39 +87,122 @@
                   '$.jobChange.job.jobStats.startTime')),
                 SECOND) / 60) AS INT64) 
       ) AS executionMinuteBuckets,
-      CAST(JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
-          '$.jobChange.job.jobStats.totalSlotMs') 
-        AS INT64) AS totalSlotMs,
-      ARRAY_LENGTH(
-        SPLIT(
+        COALESCE(
+          CAST(JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+            '$.jobInsertion.job.jobStats.totalSlotMs') AS INT64),
+          CAST(JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+            '$.jobChange.job.jobStats.totalSlotMs') AS INT64)
+        ) AS totalSlotMs,
+      COALESCE(
+        JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+          '$.jobInsertion.job.jobStats.reservationUsage.name'),
+        JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+          '$.jobChange.job.jobStats.reservationUsage.name')
+      ) AS name,
+      COALESCE(
+        JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+          '$.jobInsertion.job.jobStats.reservationUsage.slotMs'),
+        JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+          '$.jobChange.job.jobStats.reservationUsage.slotMs')
+      ) AS slotMs,
+      COALESCE(
+        JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+          '$.jobInsertion.job.jobStats.queryStats.totalProcessedBytes'),
+        JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+          '$.jobChange.job.jobStats.queryStats.totalProcessedBytes')
+      ) AS totalProcessedBytes,
+      COALESCE(
+        CAST(
           JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
-            '$.jobChange.job.jobStats.queryStats.referencedTables'), 
-        ",")
-      ) AS totalTablesProcessed,
-      ARRAY_LENGTH(
-        SPLIT(JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
-          '$.jobChange.job.jobStats.queryStats.referencedViews'),
-          ",")
-      ) AS totalViewsProcessed,
-      JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
-        '$.jobChange.job.jobStats.queryStats.totalProcessedBytes') AS totalProcessedBytes,
-      CAST(
-        JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
-          '$.jobChange.job.jobStats.queryStats.totalBilledBytes') AS INT64
+            '$.jobInsertion.job.jobStats.queryStats.totalBilledBytes') AS INT64),
+         CAST(
+          JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+            '$.jobChange.job.jobStats.queryStats.totalBilledBytes') AS INT64)
       ) AS totalBilledBytes,
-      JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
-        '$.jobChange.job.jobStats.queryStats.billingTier') AS billingTier,
-      SPLIT(
+      COALESCE(
         JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
-          '$.jobChange.job.jobStats.queryStats.referencedTables'),"/")[SAFE_OFFSET(1)] AS refTable_project_id,
-      SPLIT(
+          '$.jobInsertion.job.jobStats.queryStats.billingTier'),
         JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
-          '$.jobChange.job.jobStats.queryStats.referencedTables'),"/")[SAFE_OFFSET(3)] AS refTable_dataset_id,
-      SPLIT(
+          '$.jobChange.job.jobStats.queryStats.billingTier')
+      ) AS billingTier,
+      COALESCE(
         JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
-          '$.jobChange.job.jobStats.queryStats.referencedTables'),
-        "/"
-      )[SAFE_OFFSET(5)] AS refTable_table_id,
+          '$.jobInsertion.job.jobStats.queryStats.referencedTables'), 
+        JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+          '$.jobChange.job.jobStats.queryStats.referencedTables')
+      ) AS referencedTables,
+      COALESCE(
+        JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+          '$.jobInsertion.job.jobStats.queryStats.referencedViews'), 
+        JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+          '$.jobChange.job.jobStats.queryStats.referencedViews')
+      ) AS referencedViews,
+      COALESCE(
+        JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+          '$.jobInsertion.job.jobStats.queryStats.referencedRoutines'), 
+        JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+          '$.jobChange.job.jobStats.queryStats.referencedRoutines')
+      ) AS referencedRoutines,
+      COALESCE(
+        JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+          '$.jobInsertion.job.jobStats.queryStats.outputRowCount'),
+        JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+          '$.jobChange.job.jobStats.queryStats.outputRowCount')
+      ) as outputRowCount,
+      COALESCE(
+        JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+          '$.jobInsertion.job.jobStats.queryStats.cacheHit'), 
+        JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+          '$.jobChange.job.jobStats.queryStats.cacheHit')
+      ) as cacheHit, 
+      COALESCE(
+        CAST(JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+          '$.jobInsertion.job.jobStats.loadStats.totalOutputBytes') AS INT64),
+        CAST(JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+          '$.jobChange.job.jobStats.loadStats.totalOutputBytes') AS INT64)
+      ) AS totalLoadOutputBytes,
+      ARRAY_LENGTH(SPLIT(
+        COALESCE(
+          JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+            '$.jobInsertion.job.jobStats.queryStats.referencedTables'), 
+          JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+            '$.jobChange.job.jobStats.queryStats.referencedTables')
+        ), ",")
+      ) AS totalTablesProcessed,
+      ARRAY_LENGTH(SPLIT(
+        COALESCE(
+          JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+            '$.jobInsertion.job.jobStats.queryStats.referencedViews'), 
+          JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+            '$.jobChange.job.jobStats.queryStats.referencedViews')
+        ), ",")
+      ) AS totalViewsProcessed,
+      ARRAY_LENGTH(SPLIT(
+        COALESCE(
+          JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+            '$.jobInsertion.job.jobStats.queryStats.referencedRoutines'), 
+          JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+            '$.jobChange.job.jobStats.queryStats.referencedRoutines')
+        ), ",")
+      ) AS totalRoutinesProcessed,
+      COALESCE(
+        SPLIT(JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+          '$.jobInsertion.job.jobStats.queryStats.referencedTables'),"/")[SAFE_OFFSET(1)],
+        SPLIT(JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+          '$.jobChange.job.jobStats.queryStats.referencedTables'),"/")[SAFE_OFFSET(1)]
+      ) AS refTable_project_id,
+      COALESCE(
+        SPLIT(JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+          '$.jobInsertion.job.jobStats.queryStats.referencedTables'),"/")[SAFE_OFFSET(3)],
+        SPLIT(JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+          '$.jobChange.job.jobStats.queryStats.referencedTables'),"/")[SAFE_OFFSET(3)]
+      ) AS refTable_dataset_id,
+      COALESCE(
+        SPLIT(JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+          '$.jobInsertion.job.jobStats.queryStats.referencedTables'),"/")[SAFE_OFFSET(5)],
+        SPLIT(JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
+          '$.jobChange.job.jobStats.queryStats.referencedTables'),"/")[SAFE_OFFSET(5)]
+      ) AS refTable_table_id,
       SPLIT(
         JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
           '$.jobChange.job.jobStats.queryStats.referencedViews'),"/"
@@ -133,23 +216,6 @@
         JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
           '$.jobChange.job.jobStats.queryStats.referencedViews'),
       "/")[SAFE_OFFSET(5)] AS refView_table_id,
-      JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
-        '$.jobChange.job.jobStats.queryStats.referencedViews') AS referencedViews,
-      JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
-        '$.jobChange.job.jobStats.queryStats.referencedTables') AS referencedTables,
-      JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
-        '$.jobChange.job.jobStats.queryStats.referencedRoutines') AS referencedRoutines,
-      JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
-        '$.jobChange.job.jobStats.queryStats.reservationUsage.name') AS name,
-      JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
-        '$.jobChange.job.jobStats.queryStats.reservationUsage.slotMs') AS slotMs,
-      JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
-        '$.jobChange.job.jobStats.queryStats.outputRowCount') as outputRowCount,
-      JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
-        '$.jobChange.job.jobStats.queryStats.cacheHit') as cacheHit, 
-      CAST(JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
-        '$.jobChange.job.jobStats.loadStats.totalOutputBytes') AS INT64
-      ) AS totalLoadOutputBytes,
       /* Queries related to JobStatus
       https://cloud.google.com/bigquery/docs/reference/auditlogs/rest/Shared.Types/BigQueryAuditMetadata#jobstatus*/
       JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson,
@@ -480,7 +546,7 @@ SELECT
     AS tableCopyDestinationTableRelativePath,
   CONCAT(tableCopyproject_id, '.', tableCopydataset_id, '.', tableCopytable_id) 
     AS tableCopyDestinationTableAbsolutePath,
-  IF(eventName = "jobChange", 1, 0) AS numCopies,
+  --IF(eventName = "jobChange", 1, 0) AS numCopies,
   /* This code queries data specific to the Copy operation */ /* The following code queries data specific to the Load operation in BQ */ totalLoadOutputBytes,
   (totalLoadOutputBytes / 1000000000) AS totalLoadOutputGigabytes,
   (totalLoadOutputBytes / 1000000000) / 1000 AS totalLoadOutputTerabytes,
