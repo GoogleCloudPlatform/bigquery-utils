3cbd93ab51b3b39970cf34be28f1098858061196
Victor Hua <vhua@princeton.edu>
2020-07-08 18:22:19 -0400

    refactored Keywords and KeywordsMapping

--- tools/template_based_query_generation/src/main/java/Keywords.java
+++ tools/template_based_query_generation/src/main/java/Keywords.java
@@ -1,23 +1,81 @@
 import com.google.common.collect.ImmutableSet;
 
 /**
- * Keywords set interface that all keyword set classes implement
+ * Set of user-defined DDL, DML, and DQL keywords
  */
+public class Keywords {
 
-public interface Keywords {
+	private final String filePathDDL = "./src/main/resources/user_config/ddl.txt";
+	private final String filePathDML = "./src/main/resources/user_config/dml.txt";
+	private final String filePathDQL = "./src/main/resources/user_config/dql.txt";
+
+	private final ImmutableSet<String> setDDL;
+	private final ImmutableSet<String> setDML;
+	private final ImmutableSet<String> setDQL;
 
 	/**
-	 * Returns the set of keywords
-	 *
-	 * @return the set of keywords
+	 * Constructor of user-defined keywords, parsed from the config file
 	 */
-	public ImmutableSet<String> getKeywordsSet();
+	public Keywords() {
+		setDDL = Utils.makeImmutableSet(filePathDDL);
+		setDML = Utils.makeImmutableSet(filePathDML);
+		setDQL = Utils.makeImmutableSet(filePathDQL);
+	}
 
 	/**
-	 * Checks whether a word is in the set of user-specified keywords
+	 * Returns the set of SQL DDL keywords
+	 *
+	 * @return the set of SQL DDL keywords
+	 */
+	public ImmutableSet<String> getKeywordsDDL() {
+		return setDDL;
+	}
+
+	/**
+	 * Returns the set of SQL DML keywords
+	 *
+	 * @return the set of SQL DML keywords
+	 */
+	public ImmutableSet<String> getKeywordsDML() {
+		return setDML;
+	}
+
+	/**
+	 * Returns the set of SQL DQL keywords
+	 *
+	 * @return the set of SQL DQL keywords
+	 */
+	public ImmutableSet<String> getKeywordsDQL() {
+		return setDQL;
+	}
+
+	/**
+	 * Checks whether a word is in the DDL set of user-specified keywords
 	 *
 	 * @param word the word to check
-	 * @return a boolean indicating if the word is in the keywords set
+	 * @return a boolean indicating if the word is in the DDL keywords set
 	 */
-	public boolean inKeywordsSet(String word);
+	public boolean inKeywordsDDL(String word) {
+		return setDDL.contains(word);
+	}
+
+	/**
+	 * Checks whether a word is in the DML set of user-specified keywords
+	 *
+	 * @param word the word to check
+	 * @return a boolean indicating if the word is in the DML keywords set
+	 */
+	public boolean inKeywordsDML(String word) {
+		return setDML.contains(word);
+	}
+
+	/**
+	 * Checks whether a word is in the DQL set of user-specified keywords
+	 *
+	 * @param word the word to check
+	 * @return a boolean indicating if the word is in the DQL keywords set
+	 */
+	public boolean inKeywordsDQL(String word) {
+		return setDQL.contains(word);
+	}
 }
