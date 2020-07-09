0e6f164af159ef8383c030f20958260fc8ac4854
noah-kuo <nkuo@google.com>
2020-07-09 15:34:33 -0700

    Fixed command-line args and updated README

--- tools/unsupervised_dataset/run_crawler.py
+++ tools/unsupervised_dataset/run_crawler.py
@@ -6,11 +6,11 @@ from sql_crawler import crawler
 
 def start_crawler():
     parser = argparse.ArgumentParser(description="SQL Web Crawler")
-    parser.add_argument("urls", help="A list of URLs to be crawled", nargs='+')
-    parser.add_argument("--max_depth", help="The max depth of the crawler (default=3)", type=int, nargs=1, default=[3])
-    parser.add_argument("--max_size", help="The maximum number of links to be crawled (default=100)", type=int, nargs=1, default=[100])
+    parser.add_argument("urls", help="A space-separated list of URLs to be crawled", nargs='+')
+    parser.add_argument("--max_depth", help="The max depth of the crawler (default=3)", type=int, default=3)
+    parser.add_argument("--max_size", help="The maximum number of links to be crawled (default=100)", type=int, default=100)
     args = parser.parse_args()
-    new_crawler = crawler.Crawler(args.urls, max_size=args.max_size[0], max_depth=args.max_depth[0])
+    new_crawler = crawler.Crawler(args.urls, max_size=args.max_size, max_depth=args.max_depth)
     new_crawler.crawl()
 
 def main():
