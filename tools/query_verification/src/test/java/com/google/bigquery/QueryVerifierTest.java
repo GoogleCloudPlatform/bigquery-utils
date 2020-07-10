f7bb4528b90ac411e235814d080eddbb6d8f1e34
Krish Suchdev <krishsuchdev@google.com>
2020-07-10 20:10:50 +0000

    Support for multiple table schema and ddl

--- tools/query_verification/src/test/java/com/google/bigquery/QueryVerifierTest.java
+++ tools/query_verification/src/test/java/com/google/bigquery/QueryVerifierTest.java
@@ -3,6 +3,8 @@ package com.google.bigquery;
 import com.google.cloud.bigquery.*;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class QueryVerifierTest {
@@ -13,19 +15,41 @@ public class QueryVerifierTest {
     public void testGetTableIdFromJsonSchema() {
         String schemaContents = Main.getContentsOfFile(resourcesPath + "schema1.json");
         QueryVerificationSchema schema = QueryVerificationSchema.create(schemaContents, "");
-        TableInfo tableInfo = QueryVerifier.getTableInfoFromJsonSchema(schema);
-        TableId tableId = tableInfo.getTableId();
+        List<TableInfo> tableInfos = QueryVerifier.getTableInfoFromJsonSchema(schema);
+
+        assertEquals(tableInfos.size(), 1);
+        TableId tableId = tableInfos.get(0).getTableId();
 
         assertEquals(tableId.getDataset(), "dataset");
         assertEquals(tableId.getTable(), "table");
     }
 
     @Test
+    public void testGetMultipleTableIdFromJsonSchema() {
+        String schemaContents = Main.getContentsOfFile(resourcesPath + "schema3.json");
+        QueryVerificationSchema schema = QueryVerificationSchema.create(schemaContents, "");
+        List<TableInfo> tableInfos = QueryVerifier.getTableInfoFromJsonSchema(schema);
+
+        assertEquals(tableInfos.size(), 2);
+        TableId tableId;
+
+        tableId = tableInfos.get(0).getTableId();
+        assertEquals(tableId.getDataset(), "dataset");
+        assertEquals(tableId.getTable(), "firstTable");
+
+        tableId = tableInfos.get(1).getTableId();
+        assertEquals(tableId.getDataset(), "dataset");
+        assertEquals(tableId.getTable(), "secondTable");
+    }
+
+    @Test
     public void testGetFieldsFromJsonSchema() {
         String schemaContents = Main.getContentsOfFile(resourcesPath + "schema2.json");
         QueryVerificationSchema schema = QueryVerificationSchema.create(schemaContents, "");
-        TableInfo tableInfo = QueryVerifier.getTableInfoFromJsonSchema(schema);
-        FieldList fieldList = tableInfo.getDefinition().getSchema().getFields();
+        List<TableInfo> tableInfos = QueryVerifier.getTableInfoFromJsonSchema(schema);
+
+        assertEquals(tableInfos.size(), 1);
+        FieldList fieldList = tableInfos.get(0).getDefinition().getSchema().getFields();
 
         assertEquals(fieldList.size(), 3);
 
@@ -44,6 +68,38 @@ public class QueryVerifierTest {
         builder = Field.newBuilder("structField", StandardSQLTypeName.STRUCT, FieldList.of(subField));
         builder.setMode(Field.Mode.REPEATED);
         assertEquals(fieldList.get(2), builder.build());
+    }
+
+    @Test
+    public void testGetTableIdFromDdlSchema() {
+        String schemaContents = "CREATE TABLE dataset.table (stringField STRING, integerField INT64);";
+        QueryVerificationSchema schema = QueryVerificationSchema.create(schemaContents, "");
+        List<TableId> tableIds = QueryVerifier.getTableIdsFromDdlSchema(schema);
+
+        assertEquals(tableIds.size(), 1);
+        TableId tableId = tableIds.get(0);
+
+        assertEquals(tableId.getDataset(), "dataset");
+        assertEquals(tableId.getTable(), "table");
+    }
+
+    @Test
+    public void testGetMultipleTableIdFromDdlSchema() {
+        String schemaContents = "CREATE TABLE dataset.firstTable (stringField STRING, integerField INT64);\n" +
+                "CREATE TABLE dataset.secondTable (stringField STRING, integerField INT64);";
+        QueryVerificationSchema schema = QueryVerificationSchema.create(schemaContents, "");
+        List<TableId> tableIds = QueryVerifier.getTableIdsFromDdlSchema(schema);
+
+        assertEquals(tableIds.size(), 2);
+        TableId tableId;
+
+        tableId = tableIds.get(0);
+        assertEquals(tableId.getDataset(), "dataset");
+        assertEquals(tableId.getTable(), "firstTable");
+
+        tableId = tableIds.get(1);
+        assertEquals(tableId.getDataset(), "dataset");
+        assertEquals(tableId.getTable(), "secondTable");
     }
 
 }
