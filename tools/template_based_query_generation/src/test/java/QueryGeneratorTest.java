90da5dac443270bbc6bf54996d9845244312eb9a
Victor Hua <vhua@princeton.edu>
2020-07-10 20:29:22 -0400

    refacted QueryGenerator with .json file paths

--- tools/template_based_query_generation/src/test/java/QueryGeneratorTest.java
+++ tools/template_based_query_generation/src/test/java/QueryGeneratorTest.java
@@ -2,26 +2,24 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class QueryGeneratorTest {
 
-    @Test
-    public void test_generateQueries_1() throws Exception {
-        String[] dialectConfigPaths = new String[] {"dialect_config/ddl_dependencies.txt",
-                "dialect_config/dml_dependencies.txt",
-                "dialect_config/dql_dependencies.txt",
-                "dialect_config/root_dependencies.txt"};
-        String[] userConfigPaths = new String[] {"user_config/ddl.txt",
-                "user_config/dml.txt",
-                "user_config/dql.txt",
-                "user_config/root.txt"};
-        String mainUserConfig = "user_config/config.txt";
-        QueryGenerator qg = new QueryGenerator(dialectConfigPaths, userConfigPaths, mainUserConfig);
-        ArrayList<ArrayList<Query>> queries = qg.generateQueries(50);
-        for (ArrayList<Query> query: queries) {
-            System.out.println(query);
-        }
-    }
+	@Test
+	public void test_generateQueries_1() throws Exception {
+		String[] dialectConfigPaths = new String[]{"dialect_config/ddl_dependencies.txt",
+				"dialect_config/dml_dependencies.txt",
+				"dialect_config/dql_dependencies.txt",
+				"dialect_config/root_dependencies.txt"};
+		String[] userConfigPaths = new String[]{"user_config/ddl.json",
+				"user_config/dml.json",
+				"user_config/dql.json",
+				"user_config/root.txt"};
+		String mainUserConfig = "user_config/config.txt";
+		QueryGenerator qg = new QueryGenerator(dialectConfigPaths, userConfigPaths, mainUserConfig);
+		ArrayList<ArrayList<Query>> queries = qg.generateQueries(50);
+		for (ArrayList<Query> query : queries) {
+			System.out.println(query);
+		}
+	}
 
 }
