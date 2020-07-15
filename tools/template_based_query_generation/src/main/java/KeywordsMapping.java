b6eb5bc933f8dc48a3ddb636baa8baf2fb55e256
Victor Hua <vhua@princeton.edu>
2020-07-15 13:42:26 -0400

    moved changes from json-parser to new branch

--- tools/template_based_query_generation/src/main/java/KeywordsMapping.java
+++ tools/template_based_query_generation/src/main/java/KeywordsMapping.java
@@ -1,20 +1,18 @@
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+
+import java.io.IOException;
+import java.nio.file.Paths;
 
 public class KeywordsMapping {
 
-  private final String filePathPostgreDDL = "./src/main/resources/dialect_config/ddl_mapping_postgre.json";
-  private final String filePathPostgreDML = "./src/main/resources/dialect_config/dml_mapping_postgre.json";
-  private final String filePathPostgreDQL = "./src/main/resources/dialect_config/dql_mapping_postgre.json";
-  private final String filePathBigQueryDDL = "./src/main/resources/dialect_config/ddl_mapping_BQ.json";
-  private final String filePathBigQueryDML = "./src/main/resources/dialect_config/dml_mapping_BQ.json";
-  private final String filePathBigQueryDQL = "./src/main/resources/dialect_config/dql_mapping_BQ.json";
+  private final String filePathDDL = "./src/main/resources/dialect_config/ddl_mapping.json";
+  private final String filePathDML = "./src/main/resources/dialect_config/dml_mapping.json";
+  private final String filePathDQL = "./src/main/resources/dialect_config/dql_mapping.json";
 
-  private final ImmutableMap<String, String> mapPostgreDDL;
-  private final ImmutableMap<String, String> mapPostgreDML;
-  private final ImmutableMap<String, String> mapPostgreDQL;
-  private final ImmutableMap<String, String> mapBigQueryDDL;
-  private final ImmutableMap<String, String> mapBigQueryDML;
-  private final ImmutableMap<String, String> mapBigQueryDQL;
+  private ImmutableMap<String, ImmutableList<Mapping>> mapDDL = new ImmutableMap.Builder<String, ImmutableList<Mapping>>().build();
+  private ImmutableMap<String, ImmutableList<Mapping>> mapDML = new ImmutableMap.Builder<String, ImmutableList<Mapping>>().build();
+  private ImmutableMap<String, ImmutableList<Mapping>> mapDQL = new ImmutableMap.Builder<String, ImmutableList<Mapping>>().build();
 
   private final Keywords keywords = new Keywords();
 
@@ -22,101 +20,57 @@ public class KeywordsMapping {
    * Constructor of keywords mapping, parsed from the config file
    */
   public KeywordsMapping() {
-    mapPostgreDDL = Utils.makeImmutableMap(filePathPostgreDDL, keywords.getKeywordsDDL());
-    mapPostgreDML = Utils.makeImmutableMap(filePathPostgreDML, keywords.getKeywordsDML());
-    mapPostgreDQL = Utils.makeImmutableMap(filePathPostgreDQL, keywords.getKeywordsDQL());
-    mapBigQueryDDL = Utils.makeImmutableMap(filePathBigQueryDDL, keywords.getKeywordsDDL());
-    mapBigQueryDML = Utils.makeImmutableMap(filePathBigQueryDML, keywords.getKeywordsDML());
-    mapBigQueryDQL = Utils.makeImmutableMap(filePathBigQueryDQL, keywords.getKeywordsDQL());
+    try {
+      mapDDL = Utils.makeImmutableMap(Paths.get(filePathDDL), keywords.getKeywordsDDL());
+      mapDML = Utils.makeImmutableMap(Paths.get(filePathDML), keywords.getKeywordsDML());
+      mapDQL = Utils.makeImmutableMap(Paths.get(filePathDQL), keywords.getKeywordsDQL());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   /**
-   * Returns the PostgreSQL mapping to a DDL word
+   * Returns the list of possible PostgreSQL, BigQuery, and Token mappings to a DDL word
    *
    * @param word the DDL word to be translated
-   * @return the PostgreSQL mapping to the word
+   * @return the list of possible PostgreSQL, BigQuery, and Token mappings to the word
    * @throws IllegalArgumentException if the DDL set does not contain the word
    */
-  public String getMappingPostgreDDL(String word) throws IllegalArgumentException {
+  public ImmutableList<Mapping> getMappingDDL(String word) throws IllegalArgumentException {
     if (!keywords.inKeywordsDDL(word)) {
       throw new IllegalArgumentException("The word is not in the DDL set");
     }
 
-    return mapPostgreDDL.get(word);
+    return mapDDL.get(word);
   }
 
   /**
-   * Returns the PostgreSQL mapping to a DML word
+   * Returns the list of possible PostgreSQL, BigQuery, and Token mappings to a DML word
    *
    * @param word the DML word to be translated
-   * @return the PostgreSQL mapping to the word
+   * @return the list of possible PostgreSQL, BigQuery, and Token mappings to the word
    * @throws IllegalArgumentException if the DML set does not contain the word
    */
-  public String getMappingPostgreDML(String word) throws IllegalArgumentException {
+  public ImmutableList<Mapping> getMappingDML(String word) throws IllegalArgumentException {
     if (!keywords.inKeywordsDML(word)) {
       throw new IllegalArgumentException("The word is not in the DML set");
     }
 
-    return mapPostgreDML.get(word);
+    return mapDML.get(word);
   }
 
   /**
-   * Returns the PostgreSQL mapping to a DQL word
+   * Returns the list of possible PostgreSQL, BigQuery, and Token mappings to a DQL word
    *
    * @param word the DQL word to be translated
-   * @return the PostgreSQL mapping to the word
+   * @return the list of possible PostgreSQL, BigQuery, and Token mappings to the word
    * @throws IllegalArgumentException if the DQL set does not contain the word
    */
-  public String getMappingPostgreDQL(String word) throws IllegalArgumentException {
+  public ImmutableList<Mapping> getMappingDQL(String word) throws IllegalArgumentException {
     if (!keywords.inKeywordsDQL(word)) {
       throw new IllegalArgumentException("The word is not in the DQL set");
     }
 
-    return mapPostgreDQL.get(word);
-  }
-
-  /**
-   * Returns the BigQuery mapping to a DDL word
-   *
-   * @param word the DDL word to be translated
-   * @return the BigQuery mapping to the keyword
-   * @throws IllegalArgumentException if the DDL set does not contain the word
-   */
-  public String getMappingBigQueryDDL(String word) throws IllegalArgumentException {
-    if (!keywords.inKeywordsDDL(word)) {
-      throw new IllegalArgumentException("The word is not in the DDL set");
-    }
-
-    return mapBigQueryDDL.get(word);
-  }
-
-  /**
-   * Returns the BigQuery mapping to a DML word
-   *
-   * @param word the DML word to be translated
-   * @return the BigQuery mapping to the keyword
-   * @throws IllegalArgumentException if the DML set does not contain the word
-   */
-  public String getMappingBigQueryDML(String word) throws IllegalArgumentException {
-    if (!keywords.inKeywordsDML(word)) {
-      throw new IllegalArgumentException("The word is not in the DML set");
-    }
-
-    return mapBigQueryDML.get(word);
-  }
-
-  /**
-   * Returns the BigQuery mapping to a DQL word
-   *
-   * @param word the DQL word to be translated
-   * @return the BigQuery mapping to the keyword
-   * @throws IllegalArgumentException if the DQL set does not contain the word
-   */
-  public String getMappingBigQueryDQL(String word) throws IllegalArgumentException {
-    if (!keywords.inKeywordsDQL(word)) {
-      throw new IllegalArgumentException("The word is not in the DQL set");
-    }
-
-    return mapBigQueryDQL.get(word);
+    return mapDQL.get(word);
   }
 }
--- tools/template_based_query_generation/src/main/java/MarkovChain.java
+++ tools/template_based_query_generation/src/main/java/MarkovChain.java
@@ -1,50 +1,49 @@
-
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.HashMap;
-import java.util.Random;
+import java.util.HashSet;
 
 /**
  * Class that represents src.main.MarkovChain encoding query and keyword dependencies
  */
 public class MarkovChain<E> {
 
-	private HashSet<Node<E>> nodes;
+  private HashSet<Node<E>> nodes;
 
-	/**
-	 * constructs MarkovChain object from edge weights and random seed
-	 * @param nodes
-	 */
-	public MarkovChain(HashMap<Node<E>, HashMap<Node<E>, Double>> nodes) {
-		this.nodes = new HashSet<Node<E>>();
-		this.nodes.addAll(nodes.keySet());
-		for (Node<E> n: nodes.keySet()){
-			n.setNeighbors(nodes.get(n));
-		}
-	}
+  /**
+   * constructs MarkovChain object from edge weights and random seed
+   *
+   * @param nodes
+   */
+  public MarkovChain(HashMap<Node<E>, HashMap<Node<E>, Double>> nodes) {
+    this.nodes = new HashSet<Node<E>>();
+    this.nodes.addAll(nodes.keySet());
+    for (Node<E> n : nodes.keySet()) {
+      n.setNeighbors(nodes.get(n));
+    }
+  }
 
-	/**
-	 * constructs MarkovChain object from set of nodes and random seed
-	 * @param nodes
-	 */
-	public MarkovChain(HashSet<Node<E>> nodes) {
-		this.nodes = nodes;
-	}
+  /**
+   * constructs MarkovChain object from set of nodes and random seed
+   *
+   * @param nodes
+   */
+  public MarkovChain(HashSet<Node<E>> nodes) {
+    this.nodes = nodes;
+  }
 
-	/**
-	 *
-	 * @param start
-	 * @return list of nodes for a random walk from start node
-	 */
-	public ArrayList<E> randomWalk(Node<E> start) {
-		ArrayList<E> walk = new ArrayList<E>();
-		Node<E> current = start;
-		while (current.hasNextNode()) {
-			walk.add(current.getObj());
-			current = current.nextNode();
-		}
-		walk.add(current.getObj());
-		return walk;
-	}
+  /**
+   * @param start
+   * @return list of nodes for a random walk from start node
+   */
+  public ArrayList<E> randomWalk(Node<E> start) {
+    ArrayList<E> walk = new ArrayList<E>();
+    Node<E> current = start;
+    while (current.hasNextNode()) {
+      walk.add(current.getObj());
+      current = current.nextNode();
+    }
+    walk.add(current.getObj());
+    return walk;
+  }
 
 }
