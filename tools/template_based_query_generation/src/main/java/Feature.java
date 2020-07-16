9c40ed3a622434eac27ef7060a9f5fe3b73270f8
Allen Wang <waallen@google.com>
2020-07-16 11:23:54 -0400

    Added JSON Config Files (#101)
    * added json config files
    
    * added test config json files
    
    * moved changes from json-parser to new branch
    
    * Revert "added test config json files"
    
    This reverts commit 026468ceb5b366c31d59b6375a2ec0b7a89e35f4.
    
    * added TODOs for unit testing new classes
    
    Co-authored-by: Victor Hua <vhua@princeton.edu>
    Co-authored-by: Victor Hua <44599941+spoiledhua@users.noreply.github.com>
    Co-authored-by: Allen Wang <abmathcounts@gmail.com>

---
+++ tools/template_based_query_generation/src/main/java/Feature.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+/**
+ * Helper class that contains all keyword variants of a feature
+ */
+public class Feature {
+  private String feature;
+  private List<Mapping> allMappings;
+
+  public String getFeature() {
+    return this.feature;
+  }
+
+  public void setFeature(String feature) {
+    this.feature = feature;
+  }
+
+  public List<Mapping> getAllMappings() {
+    return this.allMappings;
+  }
+
+  public void setAllMappings(List<Mapping> allMappings) {
+    this.allMappings = allMappings;
+  }
+}
