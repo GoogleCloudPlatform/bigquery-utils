edaaa9d2b2eeb5d5910168d8e7ca71baae45755f
Victor Hua <vhua@princeton.edu>
2020-07-16 13:43:25 -0400

    added member variable comments for Feature, Mapping

--- tools/template_based_query_generation/src/main/java/Mapping.java
+++ tools/template_based_query_generation/src/main/java/Mapping.java
@@ -6,8 +6,14 @@ import java.util.List;
  * Helper class that lists PostgreSQL and BigQuery mappings and necessary tokens for all keyword variants
  */
 public class Mapping {
+
+  /* Equivalent PostgreSQL mapping to a keyword */
   private String postgre;
+
+  /* Equivalent BigQuery mapping to a keyword */
   private String bigQuery;
+
+  /* All necessary tokens for a given keyword variant */
   private List<Tokens> tokens;
 
   /**
