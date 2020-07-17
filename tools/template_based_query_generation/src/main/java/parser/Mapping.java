bc6c5995167e1a4ea611cd44e290cc58fc0907c9
Victor Hua <vhua@princeton.edu>
2020-07-17 16:34:03 -0400

    merge to master

---
+++ tools/template_based_query_generation/src/main/java/parser/Mapping.java
@@ -0,0 +1,40 @@
+import java.util.List;
+
+/**
+ * Helper class that lists PostgreSQL and BigQuery mappings and necessary tokens for all keyword variants
+ */
+public class Mapping {
+
+  /* Equivalent PostgreSQL mapping to a keyword */
+  private String postgres;
+
+  /* Equivalent BigQuery mapping to a keyword */
+  private String bigQuery;
+
+  /* All necessary tokens for a given keyword variant */
+  private List<TokenInfo> tokenInfos;
+
+  public String getPostgres() {
+    return this.postgres;
+  }
+
+  public void setPostgres(String postgres) {
+    this.postgres = postgres;
+  }
+
+  public String getBigQuery() {
+    return this.bigQuery;
+  }
+
+  public void setBigQuery(String bigQuery) {
+    this.bigQuery = bigQuery;
+  }
+
+  public List<TokenInfo> getTokenInfos() {
+    return this.tokenInfos;
+  }
+
+  public void setTokenInfos(List<TokenInfo> tokenInfos) {
+    this.tokenInfos = tokenInfos;
+  }
+}
