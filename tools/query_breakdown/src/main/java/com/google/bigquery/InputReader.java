c7c6dc2f965a5f0017d5ca51cd2c039c2789803d
Subin Lee <subinlee@google.com>
2020-07-17 21:31:53 +0000

    readAllLines used

--- tools/query_breakdown/src/main/java/com/google/bigquery/InputReader.java
+++ tools/query_breakdown/src/main/java/com/google/bigquery/InputReader.java
@@ -1,9 +1,9 @@
 package com.google.bigquery;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 /**
  * This class will take care of the input handling logic, essentially parsing the input document
@@ -16,22 +16,12 @@ public class InputReader {
    * all the queries in a string format
    */
   public static String readInput(String filename) throws IOException {
-    BufferedReader reader = null;
-    String currentLine = "";
-    try {
-      reader = new BufferedReader(new FileReader((filename)));
-    } catch (FileNotFoundException e) {
-      System.out.println("A file of this name is not found");
-    }
+    List<String> lines = Files.readAllLines(Paths.get(filename));
 
     StringBuilder sb = new StringBuilder();
-
-    while (currentLine != null) {
-      sb.append(currentLine);
-      currentLine = reader.readLine();
+    for (String line : lines) {
+      sb.append(line);
     }
-
-    reader.close();
 
     String parsedInput = sb.toString();
     int len = parsedInput.length();
