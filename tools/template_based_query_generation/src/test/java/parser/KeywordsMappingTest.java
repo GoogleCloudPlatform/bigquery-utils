6dec35c2ef17e34df20b95927de6081ed5ab4f6b
Victor Hua <vhua@princeton.edu>
2020-07-22 13:38:23 -0400

    removed outdated tests

--- tools/template_based_query_generation/src/test/java/parser/KeywordsMappingTest.java
+++ tools/template_based_query_generation/src/test/java/parser/KeywordsMappingTest.java
@@ -41,43 +41,6 @@ public class KeywordsMappingTest {
       keywordsMapping.getMappingDDL("NON KEYWORD");
     });
   }
-  // TODO (spoiledhua): fix tests here to correspond with new changes to parser.KeywordsMapping
-
-  @Test
-  public void test_getMappingPostgreDDL() {
-    KeywordsMapping km = new KeywordsMapping();
-    assertEquals("CREATE", km.getMappingDDL("DDL_CREATE"));
-    assertThrows(IllegalArgumentException.class, () -> {
-      km.getMappingDDL("NON KEYWORD");
-    });
-  }
-
-  @Test
-  public void test_getMappingBigQueryDDL() {
-    KeywordsMapping km = new KeywordsMapping();
-    assertEquals("CREATE", km.getMappingDDL("DDL_CREATE"));
-    assertThrows(IllegalArgumentException.class, () -> {
-      km.getMappingDDL("NON KEYWORD");
-    });
-  }
-
-  @Test
-  public void test_getMappingPostgreDML() {
-    KeywordsMapping km = new KeywordsMapping();
-    assertEquals("INSERT INTO", km.getMappingDML("DML_INSERT"));
-    assertThrows(IllegalArgumentException.class, () -> {
-      km.getMappingDML("NON KEYWORD");
-    });
-  }
-
-  @Test
-  public void test_getMappingBigQueryDML() {
-    KeywordsMapping km = new KeywordsMapping();
-    assertEquals("INSERT", km.getMappingDML("DML_INSERT"));
-    assertThrows(IllegalArgumentException.class, () -> {
-      km.getMappingDML("NON KEYWORD");
-    });
-  }
 
   @Test
   public void test_getMappingDML() {
