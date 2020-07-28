f03196846f71a386b4f6a8385b006326a276299a
Subin Lee <subinlee@google.com>
2020-07-28 23:51:55 +0000

    Query Breakdown: Program now executes successfully with deletion (w/o location tracking) (#119)
    * replacement and main loop draft done
    
    * refactored QueryBreakdown, organized code for PR
    
    * deletion is now working with the main loop and run
    
    * string formatted
    
    * string formatted for querybreakdown as well
    
    * comments further addressed
    
    * added this to the constructor for consistency
    
    Co-authored-by: feiling <feilingms@gmail.com>
    Co-authored-by: AayushBhatnagar <aayush.norespite@gmail.com>

--- tools/query_breakdown/src/main/java/com/google/bigquery/ReplacementLogic.java
+++ tools/query_breakdown/src/main/java/com/google/bigquery/ReplacementLogic.java
@@ -1,7 +1,6 @@
 package com.google.bigquery;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * This class implements all the replacement logic.
@@ -10,17 +9,23 @@ public class ReplacementLogic {
 
   /**
    * Given a component, provides a recommendation as to which component the input should be
-   * replaced with.
+   * replaced with. Returns a list of components that it recommends.
+   *
+   * We choose ArrayLists as the data structure because we want to impose a certain ordering
+   * with the recommendations: certain recommendations should be "better" than others
    */
-  public static String replace (String component) {
-    return "";
-  }
+  public static ArrayList<String> replace (String component, ArrayList<String> options) {
+    // simply returns the first n options
+    int n = 3;
 
-  /**
-   * Given a component, provides a recommendation as to which list of possible components the
-   * input component can be replaced by.
-   */
-  public static List<String> replaceMultiple (String component) {
-    return new ArrayList<String>();
+    if (options.size() <= n) {
+      return new ArrayList<String>(options);
+    }
+
+    ArrayList<String> result = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+      result.add(options.get(i));
+    }
+    return result;
   }
 }
