cac58b375c2be0bad4fb7f625884e1f975fd86c2
Krish Suchdev <krishsuchdev@google.com>
2020-08-05 08:04:03 -0500

    Query Verification: Reorganized BQ verification code (#134)

--- tools/query_verification/src/main/java/com/google/bigquery/QueryJobResults.java
+++ tools/query_verification/src/main/java/com/google/bigquery/QueryJobResults.java
@@ -2,18 +2,23 @@ package com.google.bigquery;
 
 import com.google.auto.value.AutoValue;
 
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Set;
+
 /**
- * Value class for query jobs. Contains the query and the query results.
- * @param <T> Type for object storing results
+ * Value class for query jobs. Contains the query, errors (if any), and the query results (if any).
  */
 @AutoValue
-public abstract class QueryJobResults<T> {
+public abstract class QueryJobResults {
 
-    public abstract String query();
-    public abstract T results();
+    public abstract String statement();
+    @Nullable public abstract QueryVerificationQuery query();
+    @Nullable public abstract String error();
+    @Nullable public abstract Set<List<Object>> results();
 
-    public static <T> QueryJobResults<T> create(String query, T results) {
-        return new AutoValue_QueryJobResults<T>(query, results);
+    public static QueryJobResults create(String statement, QueryVerificationQuery query, String error, Set<List<Object>> results) {
+        return new AutoValue_QueryJobResults(statement, query, error, results);
     }
 
 }
