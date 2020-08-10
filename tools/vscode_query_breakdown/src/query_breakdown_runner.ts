1b2f48263a2a9a2e4e19cc73847b4395f2c3ad2a
subinlee <subinlee@google.com>
2020-08-10 18:35:27 -0400

    query breakdown runner sketched

--- tools/vscode_query_breakdown/src/query_breakdown_runner.ts
+++ tools/vscode_query_breakdown/src/query_breakdown_runner.ts
@@ -4,7 +4,36 @@ import {execFile} from 'child_process';
 export class QueryBreakdownRunner {
     constructor(private execPath: string) {}
 
-    execute(args: string[], progress: Progress<message>) {
+    execute(
+        args: string[], 
+        progress: Progress<{message?: string | undefined;}>,
+        token: CancellationToken): Promise<Object[]> {
+            if (token.isCancellationRequested) {
+                return Promise.reject();
+            }
+            return new Promise<Object[]>((resolve, reject) => {
+                progress.report({message: 'Launching Query Breakdown...'});
 
+                let jsonString = '';
+                let errorMessage = '';
+                const process = execFile(this.execPath, args).on('close', exitCode => {
+                    if (!process.killed && exitCode === 0) {
+                        resolve(JSON.parse(jsonString));
+                    }
+                    else {
+                        reject();
+                    }
+                })
+                .on('error', err => reject(err));
+
+                process.stdout!.on('data', result => {
+                    jsonString += result;
+                });
+                process.stderr!.on('data', message => {
+                    errorMessage += message;
+                });
+
+                token.onCancellationRequested(() => process.kill());
+            });
     }
 }
