0634e3e431567490c0313c2c2e3710bf5a66b753
Namrata S <16926363+NamrataShah5@users.noreply.github.com>
2020-08-18 09:21:16 -0700

    Update big_query_elt_script_logging.sql

--- views/audit/big_query_elt_script_logging.sql
+++ views/audit/big_query_elt_script_logging.sql
@@ -4,7 +4,7 @@ WITH jobChangeEvent AS (
     protopayload_auditlog.authenticationInfo.principalEmail,
     resource.labels.project_id AS projectId,
     JSON_EXTRACT_SCALAR(protopayload_auditlog.metadataJson, '$.requestMetadata.callerIp') AS callerIp,
-    timestamp as eventTimestamp,
+    timestamp,
     protopayload_auditlog.serviceName,
     protopayload_auditlog.methodName,
     CONCAT(
