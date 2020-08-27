8dc4a7e26f10d8698b0f2eba3f2bcd7726211337
Victor Hua <vhua@princeton.edu>
2020-08-27 15:38:47 -0400

    revised Table junit testing

--- tools/template_based_query_generation/src/test/java/data/TableTest.java
+++ tools/template_based_query_generation/src/test/java/data/TableTest.java
@@ -1,29 +1,40 @@
 package data;
 
+import org.apache.commons.lang3.tuple.MutablePair;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TableTest {
 
   @Test
-  public void test_getName() {
-    Table t = new Table("table 1", 10);
-    Table t2 = new Table("a second table", 10);
-    assertEquals("table 1", t.getName());
-    assertEquals("a second table", t2.getName());
-  }
-
-  @Test
-  public void test_setName() {
-    Table t = new Table("t", 10);
-    t.setName("a new name");
-    assertEquals("a new name", t.getName());
-  }
-
-  @Test
   public void test_addColumn() {
+    Table t = new Table("t", 1);
+    t.addColumn("Test", DataType.STR);
+    assertEquals(new MutablePair("Test", DataType.STR), t.getSchema().get(0));
+  }
 
+  @Test
+  public void test_getRandomColumn() {
+    Table t = new Table("t", 1);
+    assertEquals("Test", t.getRandomColumn("Test", DataType.STR));
+  }
+
+  @Test
+  public void test_generateColumn() {
+    Table t = new Table("t", 1);
+    List<?> integerData = t.generateColumn(1, DataType.INTEGER);
+    assertEquals(integerData.get(0).getClass(), Integer.class);
+  }
+
+  @Test
+  public void test_generateData() {
+    Table t = new Table("t", 1);
+    t.addColumn("Test", DataType.INTEGER);
+    List<List<?>> data = t.generateData(1);
+    assertEquals(data.get(0).get(0).getClass(), Integer.class);
   }
 
 }
