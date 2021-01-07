c614d3bb83c1a8a7ff4b4269ed81ee3c14e9bb38
Vince Gonzalez <vincegonzalez@google.com>
2021-01-06 21:59:32 -0500

    update script to lower user counts and set default slots based on on-demand

--- performance_testing/jmeter/examples/run_jmeter_jdbc_sampler.sh
+++ performance_testing/jmeter/examples/run_jmeter_jdbc_sampler.sh
@@ -2,14 +2,14 @@
 
 ~/.local/apache-jmeter-5.3/bin/jmeter -n -l /tmp/sample_jmeter_log \
 -t BigQuery-BI-and-ELT.jmx \
--Jproject_id=${PROJECT_ID:=bq-workshop-160115} \
+-Jproject_id=${PROJECT_ID} \
 -Jpdt_csv_path=pdt_queries.csv \
 -Jbq_public_csv_path=bigquery_public_data_bi_queries.csv \
 -Jerror_csv_path=errors.csv \
--Jpdt_num_users=3 \
--Jbq_public_num_users=20 \
+-Jpdt_num_users=1 \
+-Jbq_public_num_users=5 \
 -Jnum_loops=1 \
--Jnum_slots=$SLOT_COUNT \
+-Jnum_slots=${SLOT_COUNT:=2000} \
 -Jrun_id="test_run_$(date +%s)" \
 -Jthread_duration=60 \
 -Jramp_time=0;
