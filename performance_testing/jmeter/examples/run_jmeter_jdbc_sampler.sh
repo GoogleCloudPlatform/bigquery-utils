7137040407f848b5b81e867dd1d4337752120712
Vince Gonzalez <vincegonzalez@google.com>
2021-01-11 17:16:28 -0500

    update copyright date, add license header to run_jmeter_jdbc_sampler.sh

--- performance_testing/jmeter/examples/run_jmeter_jdbc_sampler.sh
+++ performance_testing/jmeter/examples/run_jmeter_jdbc_sampler.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-~/.local/apache-jmeter-5.3/bin/jmeter -n -l /tmp/sample_jmeter_log \
+apache-jmeter-5.4/bin/jmeter -n -l /tmp/sample_jmeter_log \
 -t BigQuery-BI-and-ELT.jmx \
 -Jproject_id=${PROJECT_ID} \
 -Jpdt_csv_path=pdt_queries.csv \
