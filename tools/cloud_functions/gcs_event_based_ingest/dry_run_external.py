43d3ee7706cf7a9023dc00d53fc31d5244bde26e
Jacob Ferriero <jferriero@google.com>
2021-01-13 10:59:22 -0800

    fixup! module docstring for dry_run_external cli

--- tools/cloud_functions/gcs_event_based_ingest/dry_run_external.py
+++ tools/cloud_functions/gcs_event_based_ingest/dry_run_external.py
@@ -11,7 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" Command Line utility for backfilling gcs_ocn_bq_ingest cloud function
+"""Command Line utility for dry running BigQuery queries that reference
+temporary external tables over data in GCS.
 """
 import argparse
 import json
