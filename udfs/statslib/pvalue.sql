9a63ff29bb4aa9d88dd950a0eacc40c1ee570408
Ian Mathews <ian@redivis.com>
2021-02-02 15:52:05 -0800

    fix typo

--- udfs/statslib/pvalue.sql
+++ udfs/statslib/pvalue.sql
@@ -6,4 +6,5 @@ LANGUAGE js AS """
   return 1.0 - jStat['chisquare'].cdf(H, dof)
 """
 OPTIONS ( 
-    library="library=["gs://bqutil-lib/bq_js_libs/jstat-v1.9.4.min.js"]");
+    library=["gs://bqutil-lib/bq_js_libs/jstat-v1.9.4.min.js"]
+);
