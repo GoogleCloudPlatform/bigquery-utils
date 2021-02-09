14cad124ca885c5419866958b758d9ea96eec608
J Ross Thomson <39315853+jrossthomson@users.noreply.github.com>
2021-02-09 09:40:33 -0500

    Update udfs/statslib/pvalue.sql
    Co-authored-by: Daniel De Leo <danieldeleo@users.noreply.github.com>

--- udfs/statslib/pvalue.sql
+++ udfs/statslib/pvalue.sql
@@ -1,6 +1,6 @@
 #standardSQL
 
-CREATE OR REPLACE FUNCTION st.pvalue(H FLOAT64, dof INT64) 
+CREATE OR REPLACE FUNCTION fn.pvalue(H FLOAT64, dof INT64) 
 RETURNS FLOAT64 
 LANGUAGE js AS """
   return 1.0 - jStat['chisquare'].cdf(H, dof)
