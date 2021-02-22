c668b7fa13cf5eaffea1788eb94aca75551fa20b
J Ross Thomson <39315853+jrossthomson@users.noreply.github.com>
2021-02-22 10:05:46 -0500

    Update udfs/community/pvalue.sql
    Co-authored-by: Daniel De Leo <danieldeleo@users.noreply.github.com>

--- udfs/community/pvalue.sql
+++ udfs/community/pvalue.sql
@@ -3,7 +3,7 @@
 CREATE OR REPLACE FUNCTION fn.pvalue(H FLOAT64, dof INT64) 
 RETURNS FLOAT64 
 LANGUAGE js AS """
-  return 1.0 - jStat['chisquare'].cdf(H, dof)
+  return 1.0 - jstat.jStat['chisquare'].cdf(H, dof)
 """
 OPTIONS ( 
     library=["${JS_BUCKET}/jstat-v1.9.4.min.js"]
