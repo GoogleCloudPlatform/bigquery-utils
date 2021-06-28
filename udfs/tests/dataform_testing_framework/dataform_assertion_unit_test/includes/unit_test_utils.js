081688387df7039d129cf3fa73a2a655f2898e68
danieldeleo <ddeleo@google.com>
2021-06-27 23:00:11 -0400

    auto-format

--- udfs/tests/dataform_testing_framework/dataform_assertion_unit_test/includes/unit_test_utils.js
+++ udfs/tests/dataform_testing_framework/dataform_assertion_unit_test/includes/unit_test_utils.js
@@ -12,70 +12,82 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
-function generate_udf_test(test_udf, test_cases){
-    const test_name = `${test_udf}_${uuidv4()}`
-    create_dataform_test_view(test_name, test_udf, test_cases)
-    let expected_output_select_statements = [];
-    let test_input_select_statements = [];
-    test_cases.forEach(test_case => {
-        const keys = Object.keys(test_case)
-        let inputs = ""
-        keys.forEach(key_name => {
-            if(key_name.startsWith("input")){
-                inputs += `${test_case[key_name]} AS test_${key_name},\n`
-            }
-        })
-        test_input_select_statements.push(`
-          SELECT
-            ${inputs}
+function generate_udf_test(test_udf, test_cases) {
+  const test_name = `${test_udf}_${uuidv4()}`;
+  create_dataform_test_view(test_name, test_udf, test_cases);
+  let expected_output_select_statements = [];
+  let test_input_select_statements = [];
+  test_cases.forEach((test_case) => {
+    const keys = Object.keys(test_case);
+    let inputs = "";
+    keys.forEach((key_name) => {
+      if (key_name.startsWith("input")) {
+        inputs += `${test_case[key_name]} AS test_${key_name},\n`;
+      }
+    });
+    test_input_select_statements.push(`
+          SELECT ${inputs}
         `);
-        expected_output_select_statements.push(`
+    expected_output_select_statements.push(`
           SELECT ${test_case.expected_output} AS udf_output
         `);
-    })
-    run_dataform_test(test_name, test_input_select_statements, expected_output_select_statements)
+  });
+  run_dataform_test(
+    test_name,
+    test_input_select_statements,
+    expected_output_select_statements
+  );
 }
 
-function create_dataform_test_view(test_name, test_udf, test_cases){
-    const keys = Object.keys(test_cases[0])
-    let udf_input_aliases = ""
-    keys.forEach(key_name => {
-        if(key_name.startsWith("input")){
-            udf_input_aliases += `test_${key_name},`
-        }
-    })
-    udf_input_aliases = udf_input_aliases.slice(0, udf_input_aliases.length-1);
-    publish(test_name)
-        .type("view")
-        .query(ctx => `
+function create_dataform_test_view(test_name, test_udf, test_cases) {
+  const keys = Object.keys(test_cases[0]);
+  let udf_input_aliases = "";
+  keys.forEach((key_name) => {
+    if (key_name.startsWith("input")) {
+      udf_input_aliases += `test_${key_name},`;
+    }
+  });
+  udf_input_aliases = udf_input_aliases.slice(0, udf_input_aliases.length - 1);
+  publish(test_name)
+    .type("view")
+    .query(
+      (ctx) => `
             SELECT
               ${get_udf_project()}.${test_udf}(
                     ${udf_input_aliases}) AS udf_output
             FROM ${ctx.resolve("test_inputs")}
-        `);
+        `
+    );
 }
 
-function run_dataform_test(test_name, test_input_select_statements, expected_output_select_statements){
-    test(test_name)
-        .dataset(test_name)
-        .input("test_inputs", `${test_input_select_statements.join(' UNION ALL\n')}`)
-        .expect(`${expected_output_select_statements.join(' UNION ALL\n')}`);
+function run_dataform_test(
+  test_name,
+  test_input_select_statements,
+  expected_output_select_statements
+) {
+  test(test_name)
+    .dataset(test_name)
+    .input(
+      "test_inputs",
+      `${test_input_select_statements.join(" UNION ALL\n")}`
+    )
+    .expect(`${expected_output_select_statements.join(" UNION ALL\n")}`);
 }
 
-function get_udf_project(){
+function get_udf_project() {
   // This function returns the
-  return `\`${dataform.projectConfig.defaultDatabase}\``
+  return `\`${dataform.projectConfig.defaultDatabase}\``;
 }
 
 // Source: https://stackoverflow.com/a/2117523
 function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
     return v.toString(16);
   });
 }
 
 module.exports = {
-    generate_udf_test
-}
+  generate_udf_test,
+};
