72db4f510d769a45e1700531313e655559176899
Ross Thomson <jrossthomson@google.com>
2021-12-09 20:38:53 +0000

    Added license header

--- stored_procedures/bh_multiple_tests.sql
+++ stored_procedures/bh_multiple_tests.sql
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 -- Adjust p values using the Benjamini-Hochberg multipletests method, additional details in doi:10.1098/rsta.2009.0127
 -- the implementation can be compared with the python function 'statsmodels.stats.multitest.multipletests' (method='fdr_bh')
 
