dbb43d5e5721827928175ef960d5e19f9db78253
Amrish Kumar P P <p.p.amrishkumar@gmail.com>
2022-06-09 15:59:02 +0530

    fix: billing analytics script - monthly invoice costs

--- scripts/billing/sql/monthly_invoice_costs_credits.sql
+++ scripts/billing/sql/monthly_invoice_costs_credits.sql
@@ -5,11 +5,10 @@ taxes, adjustments, and rounding errors.
 
 SELECT
   invoice.month AS invoice_month,
-  SUM(cost) + SUM(IFNULL(credits.amount, 0)) AS total,
+  SUM(cost) + SUM((SELECT SUM(amount) FROM UNNEST(credits))) AS total,
   (SUM(CAST(cost * 1000000 AS INT64)) +
-    SUM(IFNULL(CAST(credits.amount * 1000000 AS INT64), 0))) / 1000000
+    SUM((SELECT SUM(CAST(amount * 1000000 AS INT64)) FROM UNNEST(credits)))) / 1000000
     AS total_exact
 FROM `bqutil.billing.billing_dashboard_export`
-LEFT JOIN UNNEST(credits) AS credits
 GROUP BY invoice_month
 ORDER BY invoice_month ASC
