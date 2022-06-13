abcf98f86a82f11afc7d74964da4357eeb6ad929
Candice Hou <89653023+candicehou07@users.noreply.github.com>
2022-06-13 16:31:24 -0400

    Update identifiers and refractor terraform_sync.py

--- tools/terraform_sync_tool/terraform_sync.py
+++ tools/terraform_sync_tool/terraform_sync.py
@@ -3,8 +3,6 @@ import io
 import argparse
 from google.cloud import bigquery
 
-
-
 # Fetch schemas for drifted tables form BQ
 def get_schemas_from_BQ(drifted_tables):
     table_schemas = []
@@ -18,40 +16,52 @@ def get_schemas_from_BQ(drifted_tables):
         table_schemas.append(schema_bq)
     return table_schemas
 
-def main():
-    parser = argparse.ArgumentParser(description='user-provided arguments')
-    parser.add_argument('filename')
-    args = parser.parse_args()
-
+# Identify tables with drifts and add return drifted_tables list 
+def get_drifted_tables(json_file):
     # Opening JSON file
-    with open(args.filename) as file:
+    with open(json_file) as file:
         lines = file.readlines()
         drifted_tables = []
-        drifted_table = ''                                                                                                    
+        drifted_table = {}                                                                                                    
         for line in reversed(lines):                                                                    
             json_line = json.loads(line)
             type = json_line.get('type')
             # Scan the json lines to detect drifts
             if type:
                 if type == 'resource_drift' and json_line.get('change').get('resource').get('resource_type') == 'google_bigquery_table':
-                    table_name = json_line.get('change').get('resource').get('resource_key')
-                    drifted_table = table_name
-                if json_line.get('type') == 'refresh_complete':
-                    resource_table = json_line.get('hook').get('id_value')
-                    if(resource_table[len(resource_table) - len(drifted_table):] == drifted_table):
+                    drifted_table = {
+                        'resource_name':json_line.get('change').get('resource').get('resource_name'),
+                        'resource_key':json_line.get('change').get('resource').get('resource_key')
+                        }
+                if json_line.get('type') == 'refresh_complete' and  json_line.get('hook').get('resource').get('resource_type') == 'google_bigquery_table':
+                    event_table = {
+                        'resource_name':json_line.get('hook').get('resource').get('resource_name'),
+                        'resource_key':json_line.get('hook').get('resource').get('resource_key')
+                    }
+                    if(drifted_table == event_table):
+                        drifted_table_name = json_line.get('hook').get('id_value')
                         # Convert table resource from terraform log output to 
                         # table_id format:[gcp_project_id].[dataset_id].[table_id]
                         table_id = ""
-                        for s in resource_table.rsplit("/"):
+                        for s in drifted_table_name.rsplit("/"):
                             if(s != "projects" and s != "datasets" and s != "tables"):
                                 table_id += s+"."
-                        drifted_tables.append(table_id[:len(table_id)-1])                        
+                        drifted_tables.append(table_id[:len(table_id)-1]) 
+    return drifted_tables
+
+def main():
+    parser = argparse.ArgumentParser(description='user-provided arguments')
+    parser.add_argument('filename')
+    args = parser.parse_args()
+
+    # Parse json file to identify drifted tables
+    drifted_tables = get_drifted_tables(args.filename)                   
         
-        if drifted_tables:
-            # Fetch latest schemas for drifted tables from BQ
-            drifted_table_schemas = get_schemas_from_BQ(drifted_tables)
-            # Drifts detected, throw exceptions
-            raise Exception("Drifts are detected in these tables, please update your terraform schema files with the following updated table schemas. ", drifted_table_schemas)
+    if drifted_tables:
+        # Fetch latest schemas for drifted tables from BQ
+        drifted_table_schemas = get_schemas_from_BQ(drifted_tables)
+        # Drifts detected, throw exceptions
+        raise Exception("Drifts are detected in these tables, please update your terraform schema files with the following updated table schemas. ", drifted_table_schemas)
 
 if __name__ == "__main__":
     main()
