c89fee761a4689a001f48a3f9a5295d185a9ce37
Candice Hou <89653023+candicehou07@users.noreply.github.com>
2022-06-26 12:16:44 -0400

    Clean out variables unused

--- tools/terraform_sync_tool/modules/bigquery/main.tf
+++ tools/terraform_sync_tool/modules/bigquery/main.tf
@@ -1,7 +1,6 @@
 locals {
   datasets        = { for dataset in var.datasets : dataset["dataset_id"] => dataset }
   tables          = { for table in var.tables : table["table_id"] => table }
-  views           = { for view in var.views : view["view_id"] => view }
 
   iam_to_primitive = {
     "roles/bigquery.dataOwner" : "OWNER"
@@ -17,30 +16,7 @@ resource "google_bigquery_dataset" "bq_dataset" {
   dataset_id      = each.key
   location        = each.value["location"] 
   project         = var.project_id
-
-  dynamic "default_encryption_configuration" {
-    for_each = var.encryption_key == null ? [] : [var.encryption_key]
-    content {
-      kms_key_name = var.encryption_key
-    }
-  }
-
-  dynamic "access" {
-    for_each = var.access
-
-    content {
-      # BigQuery API converts IAM to primitive roles in its backend.
-      # This causes Terraform to show a diff on every plan that uses IAM equivalent roles.
-      # Thus, do the conversion between IAM to primitive role here to prevent the diff.
-      role = lookup(local.iam_to_primitive, access.value.role, access.value.role)
-
-      domain         = lookup(access.value, "domain", null)
-      group_by_email = lookup(access.value, "group_by_email", null)
-      user_by_email  = lookup(access.value, "user_by_email", null)
-      special_group  = lookup(access.value, "special_group", null)
-    }
-  }
- }
+}
 
 resource "google_bigquery_table" "bq_table" {
   for_each        = local.tables
@@ -75,22 +51,5 @@ resource "google_bigquery_table" "bq_table" {
         interval = range_partitioning.value["range"].interval
       }
     }
-  }
-  
-}
-
-resource "google_bigquery_table" "bq_view" {
-  for_each      = local.views
-  dataset_id    = each.value["dataset_id"]
-  friendly_name = each.key
-  table_id      = each.key
-  labels        = each.value["labels"]
-  project       = var.project_id
-  deletion_protection = each.value["deletion_protection"]
-  depends_on = [google_bigquery_table.bq_table]
-
-  view {
-    query          = each.value["query"]
-    use_legacy_sql = each.value["use_legacy_sql"]
   }
 }
--- tools/terraform_sync_tool/modules/bigquery/variables.tf
+++ tools/terraform_sync_tool/modules/bigquery/variables.tf
@@ -1,19 +1,7 @@
-variable "description" {
-  description = "Dataset description."
-  type        = string
-  default     = null
-}
-
 variable "location" {
   description = "The regional location for the dataset only US and EU are allowed in module"
   type        = string
   default     = "US"
-}
-
-variable "delete_contents_on_destroy" {
-  description = "(Optional) If set to true, delete all the tables in the dataset when destroying the resource; otherwise, destroying the resource will fail if tables are present."
-  type        = bool
-  default     = null
 }
 
 variable "deletion_protection" {
@@ -22,45 +10,11 @@ variable "deletion_protection" {
   default     = true
   }
 
-variable "default_table_expiration_ms" {
-  description = "TTL of tables using the dataset in MS"
-  type        = number
-  default     = null
-}
-
 variable "project_id" {
   description = "Project where the dataset and table are created"
   type        = string
 }
 
-variable "encryption_key" {
-  description = "Default encryption key to apply to the dataset. Defaults to null (Google-managed)."
-  type        = string
-  default     = null
-}
-
-variable "dataset_labels" {
-  description = "Key value pairs in a map for dataset labels"
-  type        = map(string)
-  default     = {}
-}
-
-# Format: list(objects)
-# domain: A domain to grant access to.
-# group_by_email: An email address of a Google Group to grant access to.
-# user_by_email:  An email address of a user to grant access to.
-# special_group: A special group to grant access to.
-
-variable "access" {
-  description = "An array of objects that define dataset access for one or more entities."
-  type        = any
-
-  # At least one owner access is required.
-  default = [{
-    role          = "roles/bigquery.dataOwner"
-    special_group = "projectOwners"
-  }]
-}
 variable "datasets" {
   description = "this is a test DS"
   default = []
@@ -71,6 +25,7 @@ variable "datasets" {
    }
   ))
 }
+
 variable "tables" {
   description = "A list of objects which include table_id, schema, clustering, time_partitioning, expiration_time and labels."
   default     = []
@@ -98,16 +53,4 @@ variable "tables" {
     labels          = map(string),
   }
   ))
-}
-variable "views" {
-  description = "A list of objects which include table_id, which is view id, and view query"
-  default     = []
-  type = list(object({
-    view_id        = string,
-    dataset_id     = string,
-    query          = string,
-    deletion_protection=bool,
-    use_legacy_sql = bool,
-    labels         = map(string),
-  }))
 }
