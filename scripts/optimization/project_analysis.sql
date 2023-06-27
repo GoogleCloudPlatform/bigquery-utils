44dc2afdab3122d246206dbbda9fce7c0748e135
ddeleo <danieldeleo@google.com>
2023-06-27 12:09:57 -0400

    linting

--- scripts/optimization/project_analysis.sql
+++ scripts/optimization/project_analysis.sql
@@ -43,37 +43,57 @@ CREATE OR REPLACE TABLE `<dest_project_id>.<dest_dataset>.PROJECT_ANALYSIS` AS
 SELECT
   day, 
   project_id,
-  COUNT(*) as job_count,
-  AVG(total_slot_ms)/1000 avg_total_slot_secs,
-  MAX(median_total_slot_ms)/1000 median_total_slot_secs,
-  MAX(p80_total_slot_ms)/1000 p80_total_slot_secs,
-  SUM(total_slot_ms)/1000/60/60 total_slot_hours,
-  AVG(time_secs) avg_time_secs,
-  MAX(median_time_secs) median_time_secs,
-  SUM(time_secs)/60/60 total_time_hours,
-  MAX(p80_time_secs) p80_time_secs,
-  AVG(bytes_scanned)/POW(1024,3) avg_gb_scanned,
-  MAX(p80_bytes_scanned)/POW(1024,3) p80_gb_scanned,
-  SUM(bytes_scanned)/POW(1024,4) total_tb_scanned,
-  AVG(bytes_shuffled)/POW(1024,3) avg_gb_shuffled,
-  MAX(p80_bytes_shuffled)/POW(1024,3) p80_gb_shuffled,
-  SUM(bytes_shuffled)/POW(1024,4) total_tb_shuffled,
-  AVG(bytes_spilled)/POW(1024,3) avg_gb_spilled,
-  MAX(p80_bytes_spilled)/POW(1024,3) p80_gb_spilled,
-  SUM(bytes_spilled)/POW(1024,4) total_tb_spilled,
+  COUNT(*)                            AS job_count,
+  AVG(total_slot_ms)/1000             AS avg_total_slot_secs,
+  MAX(median_total_slot_ms)/1000      AS median_total_slot_secs,
+  MAX(p80_total_slot_ms)/1000         AS p80_total_slot_secs,
+  SUM(total_slot_ms)/1000/60/60       AS total_slot_hours,
+  AVG(time_secs)                      AS avg_time_secs,
+  MAX(median_time_secs)               AS median_time_secs,
+  SUM(time_secs)/60/60                AS total_time_hours,
+  MAX(p80_time_secs)                  AS p80_time_secs,
+  AVG(bytes_scanned)/POW(1024,3)      AS avg_gb_scanned,
+  MAX(p80_bytes_scanned)/POW(1024,3)  AS p80_gb_scanned,
+  SUM(bytes_scanned)/POW(1024,4)      AS total_tb_scanned,
+  AVG(bytes_shuffled)/POW(1024,3)     AS avg_gb_shuffled,
+  MAX(p80_bytes_shuffled)/POW(1024,3) AS p80_gb_shuffled,
+  SUM(bytes_shuffled)/POW(1024,4)     AS total_tb_shuffled,
+  AVG(bytes_spilled)/POW(1024,3)      AS avg_gb_spilled,
+  MAX(p80_bytes_spilled)/POW(1024,3)  AS p80_gb_spilled,
+  SUM(bytes_spilled)/POW(1024,4)      AS total_tb_spilled,
 FROM(
   SELECT
-    day, project_id,
-    total_slot_ms, PERCENTILE_CONT(total_slot_ms, 0.5) OVER (PARTITION BY day, project_id) as median_total_slot_ms, PERCENTILE_CONT(total_slot_ms, 0.8) OVER (PARTITION BY day, project_id) as p80_total_slot_ms,
-    time_secs, PERCENTILE_CONT(time_secs, 0.5) OVER (PARTITION BY day, project_id) as median_time_secs, PERCENTILE_CONT(time_secs, 0.8) OVER (PARTITION BY day, project_id) as p80_time_secs,
-    total_bytes_scanned bytes_scanned, PERCENTILE_CONT(total_bytes_scanned, 0.8) OVER (PARTITION BY day, project_id) as p80_bytes_scanned,
-    bytes_shuffled, PERCENTILE_CONT(bytes_shuffled, 0.8) OVER (PARTITION BY day, project_id) as p80_bytes_shuffled,
-    bytes_spilled, PERCENTILE_CONT(bytes_spilled, 0.8) OVER (PARTITION BY day, project_id) as p80_bytes_spilled
+    day,
+    project_id,
+    total_slot_ms,
+    PERCENTILE_CONT(total_slot_ms, 0.5) 
+      OVER (PARTITION BY day, project_id)     AS median_total_slot_ms,
+    PERCENTILE_CONT(total_slot_ms, 0.8) 
+      OVER (PARTITION BY day, project_id)     AS p80_total_slot_ms,
+    time_secs,
+    PERCENTILE_CONT(time_secs, 0.5) 
+      OVER (PARTITION BY day, project_id)     AS median_time_secs,
+    PERCENTILE_CONT(time_secs, 0.8) 
+      OVER (PARTITION BY day, project_id)     AS p80_time_secs,
+    total_bytes_scanned bytes_scanned,
+    PERCENTILE_CONT(total_bytes_scanned, 0.8) 
+      OVER (PARTITION BY day, project_id)     AS p80_bytes_scanned,
+    bytes_shuffled,
+    PERCENTILE_CONT(bytes_shuffled, 0.8) 
+      OVER (PARTITION BY day, project_id)     AS p80_bytes_shuffled,
+    bytes_spilled,
+    PERCENTILE_CONT(bytes_spilled, 0.8) 
+      OVER (PARTITION BY day, project_id)     AS p80_bytes_spilled
   FROM(
-    SELECT DATE(jbo.creation_time,"US/Central") as day, project_id, job_id,
-      total_slot_ms, TIMESTAMP_DIFF(jbo.end_time,jbo.creation_time, SECOND) time_secs, total_bytes_billed total_bytes_scanned,
-      (SELECT SUM(stage.shuffle_output_bytes) FROM UNNEST(job_stages) stage) bytes_shuffled,
-      (SELECT SUM(stage.shuffle_output_bytes_spilled) FROM UNNEST(job_stages) stage) bytes_spilled
+    SELECT 
+      DATE(jbo.creation_time,"US/Central") AS day,
+      project_id,
+      job_id,
+      total_slot_ms,
+      TIMESTAMP_DIFF(jbo.end_time,jbo.creation_time, SECOND) AS time_secs,
+      total_bytes_billed total_bytes_scanned,
+      (SELECT SUM(stage.shuffle_output_bytes) FROM UNNEST(job_stages) stage) AS bytes_shuffled,
+      (SELECT SUM(stage.shuffle_output_bytes_spilled) FROM UNNEST(job_stages) stage) AS bytes_spilled
     FROM
       `region-us`.INFORMATION_SCHEMA.JOBS_BY_ORGANIZATION jbo
     WHERE
