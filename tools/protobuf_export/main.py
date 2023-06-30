3931bcfd183ed81c586ec6e7a0f856c173058076
ron-gal <125445217+ron-gal@users.noreply.github.com>
2023-06-29 21:28:49 -0400

    Add example in Python

---
+++ tools/protobuf_export/main.py
@@ -0,0 +1,27 @@
+"""Queries Google BigQuery."""
+
+from google.cloud import bigquery
+from path.to.proto import awesome_pb2
+
+
+def main():
+  project_id = "your-project-id"
+  client = bigquery.Client(project=project_id)
+  query_job = client.query(query="""
+               SELECT
+				concat(word , ":",corpus) as RowKey,
+				  test.toProto(STRUCT(word, 
+              CAST(word_count AS BIGNUMERIC)),
+					"protopackage.TestMessage") AS ProtoResult
+				FROM
+				  `bigquery-public-data.samples.shakespeare`
+					ORDER BY word_count DESC
+				LIMIT 20
+    """)
+  rows = query_job.result()
+  for row in rows:
+    message = awesome_pb2.DummyMessage()
+    message.ParseFromString(row.get("ProtoResult"))
+    print(
+        "rowKey: {}, message: {}".format(row.get("RowKey"), message.dummy_field)
+    )
