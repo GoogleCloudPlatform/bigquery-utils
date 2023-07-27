3e999f938a9875517e34fa28ffea8acdced5dd60
Christian Yarros <yarros@google.com>
2023-07-27 11:40:14 -0400

    update largest tables scripts

--- scripts/optimization/largest_freq_read_tables_without_partitioning_or_clustering.sql
+++ scripts/optimization/largest_freq_read_tables_without_partitioning_or_clustering.sql
@@ -39,7 +39,9 @@ DECLARE projects ARRAY<STRING> DEFAULT (
 CREATE SCHEMA IF NOT EXISTS optimization_workshop;
 CREATE OR REPLACE TABLE optimization_workshop.largest_freq_read_tables_without_part_clust
 (
-  full_table_id STRING,
+  table_catalog STRING,
+  table_schema STRING,
+  table_name STRING,
   table_url STRING,
   is_clustered BOOLEAN,
   is_partitioned BOOLEAN,
@@ -57,26 +59,40 @@ BEGIN
   EXECUTE IMMEDIATE FORMAT("""
   INSERT INTO optimization_workshop.largest_freq_read_tables_without_part_clust
   SELECT
-    full_table_id,
-    `bigquery-public-data`.persistent_udfs.table_url(full_table_id) AS table_url,
-    EXISTS(SELECT * FROM UNNEST(clustering) AS c WHERE c <> "NULL") is_clustered,
-    EXISTS(SELECT * FROM UNNEST(partitioning) AS p WHERE p = "YES") is_partitioned,
-    SUM(SAFE_DIVIDE(total_logical_bytes, pow(2,30))) AS logical_gigabytes,
-    SUM(SAFE_DIVIDE(total_logical_bytes, pow(2,40))) AS logical_terabytes,
+    s.table_catalog,
+    s.table_schema,
+    s.table_name,
+    `bigquery-public-data`.persistent_udfs.table_url(s.table_catalog || '.' || s.table_schema || '.' || s.table_name) AS table_url,
+    EXISTS(SELECT * FROM UNNEST(c.clustering) AS c WHERE c <> "NULL") is_clustered,
+    EXISTS(SELECT * FROM UNNEST(c.partitioning) AS p WHERE p = "YES") is_partitioned,
+    SUM(SAFE_DIVIDE(s.total_logical_bytes, POW(2,30))) AS logical_gigabytes,
+    SUM(SAFE_DIVIDE(s.total_logical_bytes, POW(2,40))) AS logical_terabytes,
   FROM
-    `region-us.INFORMATION_SCHEMA.TABLE_STORAGE_BY_ORGANIZATION`
+    `region-us.INFORMATION_SCHEMA.TABLE_STORAGE_BY_ORGANIZATION` s
   JOIN (
     SELECT 
+      table_catalog,
+      table_schema,
+      table_name,
       table_catalog || '.' || table_schema || '.' || table_name AS full_table_id,
       ARRAY_AGG(COALESCE(CAST(CLUSTERING_ORDINAL_POSITION AS STRING), "NULL")) AS clustering,
       ARRAY_AGG(IS_PARTITIONING_COLUMN) AS partitioning 
-    FROM `%s.region-us.INFORMATION_SCHEMA.COLUMNS`
-    GROUP BY 1
-    ) ON (project_id = table_catalog AND table_schema = table_schema AND table_name = table_name)
+    FROM `%s.region-us.INFORMATION_SCHEMA.COLUMNS` 
+    GROUP BY 
+      table_catalog,
+      table_schema,
+      table_name
+    ) c ON (s.project_id = c.table_catalog AND s.table_schema = c.table_schema AND s.table_name = c.table_name)
   WHERE
     NOT EXISTS(SELECT * FROM UNNEST(clustering) AS c WHERE c <> "NULL")
     OR NOT EXISTS(SELECT * FROM UNNEST(partitioning) AS p WHERE p = "YES")
-  GROUP BY 1,2,3,4
+  GROUP BY
+    s.table_catalog,
+    s.table_schema,
+    s.table_name,
+    table_url,
+    is_clustered,
+    is_partitioned
   ORDER BY 5 DESC
   LIMIT 100
   ;
