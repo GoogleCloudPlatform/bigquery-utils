1c30f317024776c42d3555e618cb73356c76a0a2
ddeleo <danieldeleo@google.com>
2023-08-04 17:29:00 -0400

    run table_read_patterns script before other scripts

--- scripts/optimization/run_all_scripts.sh
+++ scripts/optimization/run_all_scripts.sh
@@ -3,7 +3,15 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+# Run the table_read_patterns.sql file first because it's a dependency for
+# some of the other scripts.
+bq query --use_legacy_sql=false --nouse_cache < table_read_patterns.sql
+
 # Run all the .sql files in the current directory
 for f in *.sql; do
+  if [ $f = "table_read_patterns.sql" ]; then
+    # Skip this file, it's already been run
+    continue
+  fi
   bq query --use_legacy_sql=false --nouse_cache < $f
 done
