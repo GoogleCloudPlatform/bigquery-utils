d23a5b843bdfb6bcf0621a5c006e1b8b2a429a13
Franklin Whaite <whaite@google.com>
2024-03-24 11:22:50 -0500

    fixing bug in column names

--- scripts/optimization/anti_pattern_recoginition_tool_tables.sql
+++ scripts/optimization/anti_pattern_recoginition_tool_tables.sql
@@ -26,8 +26,8 @@ CREATE OR REPLACE TABLE optimization_workshop.antipattern_output_table (
 
 CREATE OR REPLACE VIEW optimization_workshop.antipattern_tool_input_view AS
 SELECT 
-  query_hash id, 
-  top_10_jobs[SAFE_OFFSET(0)].query_text query 
+  Query_Hash id, 
+  Query_Raw_Sample query 
 FROM 
   optimization_workshop.viewable_queries_grouped_by_hash 
 ORDER BY 
--- scripts/optimization/update_queries_by_hash_w_anti_patterns.sql
+++ scripts/optimization/update_queries_by_hash_w_anti_patterns.sql
@@ -20,4 +20,4 @@ ADD COLUMN recommendation ARRAY<STRUCT<name STRING, description STRING>>;
 UPDATE optimization_workshop.viewable_queries_grouped_by_hash t1
 SET t1.recommendation = t2.recommendation
 FROM optimization_workshop.antipattern_output_table t2
-WHERE t1.query_hash = t2.job_id;
+WHERE t1.Query_Hash = t2.job_id;
