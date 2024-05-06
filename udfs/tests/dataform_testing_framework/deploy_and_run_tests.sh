21d6325700ea0bbb5c7d10e8d7ff97d87240c13d
ddeleo <danieldeleo@google.com>
2024-05-06 17:12:32 -0400

    Add printf during testing phase

--- udfs/tests/dataform_testing_framework/deploy_and_run_tests.sh
+++ udfs/tests/dataform_testing_framework/deploy_and_run_tests.sh
@@ -169,6 +169,7 @@ test_udfs() {
     cp "${udfs_source_dir}"/test_cases.js "${udfs_target_dir}"/definitions/test_cases.js
     generate_dataform_config_and_creds "${project_id}" "${dataset_id}" "${udfs_target_dir}"
     add_symbolic_dataform_dependencies "${udfs_target_dir}"
+    printf "Testing UDFs using dataform test command\n"
     if ! (cd "${udfs_target_dir}" && dataform test .); then
       # If any error occurs when testing, delete BigQuery testing dataset before exiting with status code 1.
       # If SHORT_SHA is not null, then we know a test dataset was used.
