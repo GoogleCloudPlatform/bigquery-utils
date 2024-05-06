fad13ca1183a260e64348be129415c79115419d1
ddeleo <danieldeleo@google.com>
2024-05-06 16:42:58 -0400

    changing into target dir before running dataform

--- udfs/tests/dataform_testing_framework/deploy_and_run_tests.sh
+++ udfs/tests/dataform_testing_framework/deploy_and_run_tests.sh
@@ -131,9 +131,10 @@ deploy_udfs() {
   replace_js_udf_bucket_placeholder "${udfs_target_dir}"/definitions/"${udf_dir}" "${js_bucket}"
   generate_dataform_config_and_creds "${project_id}" "${dataset_id}" "${udfs_target_dir}"
   add_symbolic_dataform_dependencies "${udfs_target_dir}"
+  cd "${udfs_target_dir}"
 
-  printf "Deploying UDFs using dataform run command:\n%s\n" "dataform run ${udfs_target_dir}"
-  if ! dataform run "${udfs_target_dir}"; then
+  printf "Deploying UDFs using dataform run command\n"
+  if ! dataform run .; then
     # If any error occurs, delete BigQuery testing dataset before exiting with status code 1
     # If SHORT_SHA is not null, then we know a test dataset was used.
     if [[ -n "${SHORT_SHA}" ]]; then
