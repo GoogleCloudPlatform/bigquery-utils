0ded461871cfaeba6e23eafc1fb28521a9f5c803
Daniel De Leo <danieldeleo@users.noreply.github.com>
2025-05-15 01:16:15 -0400

    newline is not playing nice with unit test

--- udfs/community/test_cases.js
+++ udfs/community/test_cases.js
@@ -3756,7 +3756,7 @@ generate_udf_test("cw_xml_extract", [
 generate_udf_test("gunzip", [
   {
     inputs: [`FROM_BASE64("H4sIACYJJWgAA8tIzcnJVyjPL8pJ4QIALTsIrwwAAAA=")`],
-    expected_output: `CAST("hello world\n" AS BYTES)`,
+    expected_output: `FROM_BASE64("aGVsbG8gd29ybGQK")`,
   },
   {
     inputs: [`CAST(NULL AS BYTES)`],
